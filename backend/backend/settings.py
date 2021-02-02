25c256cd05213fce683aff631b8da8ab887bedda
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-02 18:05:16 +0100

    move TokenAuth above SessionAuth in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
