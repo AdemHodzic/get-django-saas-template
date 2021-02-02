2edb8a4bc1a3b6e93e02f5bc1e7b6e545a46493e
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-02 18:05:37 +0100

    add basic user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra):
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, **extra):
+        user = self.create_user(
+            email,
+            password=password,
+            **extra
+        )
+        user.is_admin = True
+        user.save(using=self.db)
+        return user
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    avatar = models.ImageField(verbose_name='Avatar', blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
