8d430f1c9035f4f2d170bc218425b746b0f650bd
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-04 16:50:12 +0100

    add superuser in User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -45,3 +45,8 @@ class User(AbstractUser):
     @property
     def is_staff(self):
         return self.is_admin
+    
+    @property
+    def is_superuser(self):
+        return self.is_admin
+    
