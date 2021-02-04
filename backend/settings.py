f200333fd4177ad9bf192b9941c3918683495e5e
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-04 17:59:43 +0100

    add guards for default layout

--- backend/settings.py
+++ backend/settings.py
@@ -131,8 +131,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ]
 }
