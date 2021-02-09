f01cde142c9daa1313b03cc6f165b6cd79562e94
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-09 18:07:01 +0100

    rename backend app to config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
