ba5cfd6a8c0c991a46032272481d24182c4a1ac6
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-16 17:37:37 +0100

    add url for activating an user

--- backend/emails/views.py
+++ backend/emails/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.utils.http import urlsafe_base64_decode
+
+from users.models import User
+
+from .tokens import account_activation_token
+
+def activate(request, id, token):
+  try:
+    uid = urlsafe_base64_decode(id).decode()
+    user = User.objects.get(pk=uid)
+    
+    if user is None or not account_activation_token.check_token(user, token):
+      raise Exception('Invalid User')
+    
+    user.confirmed_email = True
+    user.save()
+    return HttpResponse('Hello World')
+  except Exception as e:
+    print(e)
+    return HttpResponse('Invalid url', status=400)
