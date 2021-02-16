0e79db22a6b1b13f1f0d8f93ee4a2203f9027c53
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-16 17:57:27 +0100

    add templates for successful and failed user confirmation

--- emails/urls.py
+++ emails/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'emails'
 
 urlpatterns = [
-  path('activate/<str:id>/<str:token>/', views.activate, name='activate')
+  path('activate/<str:id>/<str:token>/', views.activate, name='activate'),
+  path('activate/success/', views.activate_success, name='activate_success'),
+  path('activate/fail/', views.activate_fail, name='activate_fail'),
 ]
