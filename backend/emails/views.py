bea007210b09fbc70922419cba27c8b9483c855c
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-16 17:57:27 +0100

    add templates for successful and failed user confirmation

--- backend/emails/views.py
+++ backend/emails/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 from django.utils.http import urlsafe_base64_decode
@@ -17,7 +17,13 @@ def activate(request, id, token):
     
     user.confirmed_email = True
     user.save()
-    return HttpResponse('Hello World')
+    return redirect('emails:activate_success')
   except Exception as e:
     print(e)
-    return HttpResponse('Invalid url', status=400)
+    return redirect('emails:activate_fail')
+
+def activate_success(request):
+  return render(request, 'emails/activate_success.html')
+
+def activate_fail(request):
+  return render(request, 'emails/activate_fail.html')
