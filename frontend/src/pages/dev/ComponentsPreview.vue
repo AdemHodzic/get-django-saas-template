5ebabaf2044d2c2bb50a46f7df8e2145966fe6e6
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-23 17:08:01 +0100

    divide component areas in component preview page

--- frontend/src/pages/dev/ComponentsPreview.vue
+++ frontend/src/pages/dev/ComponentsPreview.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="components-preview">
+    <h1 class="area-title">Alerts</h1>
+    <hr />
     <AlertBox title="This is example title">This is example body</AlertBox>
+
+    <h1 class="area-title">Toasts</h1>
+    <hr />
     <AtomsToastSuccess title="Success!">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
     </AtomsToastSuccess>
@@ -10,9 +15,15 @@
     <AtomsToastWarning title="Warning!">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
     </AtomsToastWarning>
+
+    <h1 class="area-title">Card</h1>
+    <hr />
     <Card>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
     </Card>
+
+    <h1 class="area-title">Buttons</h1>
+    <hr />
     <div class="buttons-container">
       <AtomsBaseButton>Lorem Impsum</AtomsBaseButton>
       <AtomsPrimaryButton>Lorem Impsum</AtomsPrimaryButton>
@@ -20,6 +31,9 @@
       <AtomsDangerButton>Lorem Impsum</AtomsDangerButton>
       <AtomsWarningButton>Lorem Impsum</AtomsWarningButton>
     </div>
+
+    <h1 class="area-title">Inputs</h1>
+    <hr />
     <AtomsTextInput placeholder="Elon Musk" />
   </div>
 </template>
@@ -66,6 +80,10 @@ export default {
   .buttons-container {
     @apply flex;
     gap: 8px;
+  }
+
+  .area-title {
+    @apply font-bold text-4xl;
   }
 </style>
 
