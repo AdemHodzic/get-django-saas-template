4eb662957c54362ca43bf7d034ecc66aea552ae7
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-23 15:55:18 +0100

    add card atom

---
+++ frontend/src/components/atoms/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AtomsCard'
+}
+</script>
+
+<style>
+  .card {
+    @apply bg-white rounded-md shadow-xl p-16 max-w-xl
+  }
+</style>
