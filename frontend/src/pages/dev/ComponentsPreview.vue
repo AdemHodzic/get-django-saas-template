a9b10ea7811290b13fd6b510650878d86ae36427
HodzicAdem <adem.hodzic99@gmail.com>
2021-02-23 16:46:28 +0100

    add button components to the component preview

--- frontend/src/pages/dev/ComponentsPreview.vue
+++ frontend/src/pages/dev/ComponentsPreview.vue
@@ -13,6 +13,13 @@
     <Card>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
     </Card>
+    <div class="buttons-container">
+      <AtomsBaseButton>Lorem Impsum</AtomsBaseButton>
+      <AtomsPrimaryButton>Lorem Impsum</AtomsPrimaryButton>
+      <AtomsSuccessButton>Lorem Impsum</AtomsSuccessButton>
+      <AtomsDangerButton>Lorem Impsum</AtomsDangerButton>
+      <AtomsWarningButton>Lorem Impsum</AtomsWarningButton>
+    </div>
   </div>
 </template>
 
@@ -24,6 +31,12 @@ import AtomsToastSuccess from '@/components/atoms/toast-success.vue'
 import AtomsToastDanger from '@/components/atoms/toast-danger.vue'
 import AtomsToastWarning from '@/components/atoms/toast-warning.vue'
 
+import AtomsBaseButton from '@/components/atoms/buttons/base-button.vue'
+import AtomsPrimaryButton from '@/components/atoms/buttons/primary-button.vue'
+import AtomsSuccessButton from '@/components/atoms/buttons/success-button.vue'
+import AtomsDangerButton from '@/components/atoms/buttons/danger-button.vue'
+import AtomsWarningButton from '@/components/atoms/buttons/warning-button.vue'
+
 export default {
   name: 'ComponentsPreviewPage',
   components: {
@@ -32,6 +45,11 @@ export default {
     AtomsToastSuccess,
     AtomsToastDanger,
     AtomsToastWarning,
+    AtomsBaseButton,
+    AtomsPrimaryButton,
+    AtomsSuccessButton,
+    AtomsDangerButton,
+    AtomsWarningButton,
   },
 }
 </script>
@@ -39,6 +57,11 @@ export default {
 <style>
   .components-preview > * {
     @apply mb-4;
+  }
+
+  .buttons-container {
+    @apply flex;
+    gap: 8px;
   }
 </style>
 
