6d3080be68d99d16179841025c58a6a9a3f0e63b
HodzicAdem <adem.hodzic99@gmail.com>
2021-03-11 21:22:08 +0100

    add table component

---
+++ frontend/src/components/atoms/table/table.vue
@@ -0,0 +1,41 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <slot name="thead"></slot>
+      </tr>
+    </thead>
+    <tbody>
+      <slot></slot>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'AtomsTable'
+}
+</script>
+
+<style>
+  table {
+    @apply w-full table-auto border-collapse shadow-sm;
+  }
+
+  thead tr {
+    @apply text-left text-sm text-white bg-gray-700;
+  }
+
+  thead tr th,
+  tbody tr td {
+    @apply p-2;
+  }
+
+  tbody tr:nth-of-type(odd) {
+    @apply bg-gray-200;
+  }
+  tbody tr:nth-of-type(even) {
+    @apply bg-white;
+  }
+  
+</style>
