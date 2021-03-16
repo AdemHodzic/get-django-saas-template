b05326a7f32c62dec660a6b0bcf651953c93d852
HodzicAdem <adem.hodzic99@gmail.com>
2021-03-16 21:36:02 +0100

    add tab component

--- frontend/src/components/atoms/tab-bar.vue
+++ frontend/src/components/atoms/tab-bar.vue
@@ -11,9 +11,11 @@
       </div>
     </div>
     <div class="tab-bar__body">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
-
-      <!-- <slot v-for="tab in tabs" :key="tab" :name="tab"></slot> -->
+      <template v-for="tab in tabs">
+        <template v-if="tab === selectedTab">
+          <slot  :name="tab"></slot>
+        </template>
+      </template>
     </div>
   </div>
 </template>
--- frontend/src/pages/dev/ComponentsPreview.vue
+++ frontend/src/pages/dev/ComponentsPreview.vue
@@ -85,7 +85,30 @@
 
     <h1 class="area-title">Tab bar</h1>
     <hr />
-    <AtomsTabBar :tabs="['first', 'second', 'third']"></AtomsTabBar>
+    <AtomsTabBar :tabs="['first', 'second', 'third']">
+      <template v-slot:first>
+        <h1>
+          First Slot Content
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
+
+      </template>
+
+      <template v-slot:second>
+      <h1>
+          Second Slot Content
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
+      </template>
+
+      <template v-slot:third>
+      <h1>
+          Third Slot Content
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Pharetra vel turpis nunc eget lorem dolor sed. Et netus et malesuada fames ac. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Bibendum neque egestas congue quisque egestas diam in arcu. Augue neque gravida in fermentum et sollicitudin ac orci. Etiam erat velit scelerisque in dictum. Aliquam id diam maecenas ultricies mi eget.</p>
+      </template>
+
+    </AtomsTabBar>
   </div>
 </template>
 
