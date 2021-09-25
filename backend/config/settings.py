3b77a556f6d261555e3b6ab39b4a692af517d1d1
Adem Hodzic <athos.blade@gmail.com>
2021-09-25 16:59:10 +0200

    feat(database): use psql instad of sqlite

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST', 'localhost'),
+        'PORT': env.str('DB_PORT', '5432'),
     }
 }
 
