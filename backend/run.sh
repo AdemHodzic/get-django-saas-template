2eff6cb434122d0820b19ec4caf0f28f70d51078
Adem Hodzic <athos.blade@gmail.com>
2021-10-05 13:24:27 +0200

    update heroku.yml

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,1 @@
-python manage.py makemigrations &&
-python manage.py migrate &&
-python manage.py runserver 0.0.0.0
+bash -c "python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0"
